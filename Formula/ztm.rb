4bcd439b0dcfb67553898a37be5028001d962dbc
Ali Naqvi <syed.alinaqvi@gmail.com>
2025-09-05 20:14:46 +0800

    ztm v1.0.0-rc3

--- Formula/ztm.rb
+++ Formula/ztm.rb
@@ -1,7 +1,7 @@
 class Ztm < Formula
   desc "Zerto Trust Mesh (ZTM) is open-source software for decentralized HTTP/2 tunnels"
   homepage "https://github.com/flomesh-io/ztm"
-  url "https://github.com/flomesh-io/ztm.git", tag: "v1.0.0-rc-3"
+  url "https://github.com/flomesh-io/ztm.git", tag: "v1.0.0-rc3"
   license "Apache-2.0"
 
   bottle do
